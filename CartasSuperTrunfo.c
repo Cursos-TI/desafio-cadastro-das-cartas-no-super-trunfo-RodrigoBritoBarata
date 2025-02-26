097a1365f1b43f1e83ff5fab9f90338f799e1525
RodrigoBritoBarata <krodrigo626@gmail.com>
2025-02-26 17:52:00 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int Populaçao, NPT; float PIB, Area; char Estado[50], CDC[50], nome[50];
+    int Populaçao, NPT, Populaçaob, NPTb; float PIB, Area, PIBb, Areab; char Estado[50], CDC[50], nome[50], Estadob[50], CDCb[50], nomeb[50];
 
     //Pergunta:
     printf("Coloque o Código da carta: \n");
@@ -52,8 +52,60 @@ int main(){
     //Resposta:
     printf("\n População: %d \n", Populaçao);
 
+    //Pergunta:
+    printf("Coloque o Código da carta: \n");
+    scanf("%s", &CDCb);
+
+    //Resposta:
+    printf("\n Código da carta: %s \n", CDCb);
+
+    //Pergunta:
+    printf("\n Coloque o Estado: \n");
+    scanf(" %s", &Estadob);
+
+    //Resposta:
+    printf("\n Estado: %s \n", Estadob);
+
+    //Pergunta:
+    printf("\n Coloque a Área: \n");
+    scanf("%f", &Areab);
+
+    //Resposta:
+    printf("\n Área: %f km² \n", Areab);
+
+    //Pergunta:
+    printf("\n Coloque o Número de pontos turísticos: \n");
+    scanf("%d", &NPTb);
+
+    //Resposta:
+    printf("\n Número de pontos turísticos: %d \n", NPTb);
+
+    //Pergunta:
+    printf("\n Coloque o PIB: \n");
+    scanf("%f", &PIBb);
+
+    //Resposta:
+    printf("\n PIB: %f bilhões de reais \n", PIBb);
+
+    //Pergunta:
+    printf("\n Coloque o Nome da Cidade: \n");
+    scanf("%s", &nomeb);
+
+    //Resposta:
+    printf("\n Nome da Cidade: %s \n", nomeb);
+
+    //Pergunta:
+    printf("\n Coloque a População: \n");
+    scanf("%d", &Populaçaob);
+
+    //Resposta:
+    printf("\n População: %d \n", Populaçaob);
+
     //Resultado completo.
     printf("\n Nome da cidade: %s \n Estado: %s \n Código da Carta: %s \n Área: %f km² \n PIB: %f bilhões de reais \n População: %d \n Número de pontos turísticos: %d \n", nome, Estado, CDC, Area, PIB, Populaçao, NPT);
+
+    //Resultado completo.
+    printf("\n Nome da cidade: %s \n Estado: %s \n Código da Carta: %s \n Área: %f km² \n PIB: %f bilhões de reais \n População: %d \n Número de pontos turísticos: %d \n", nomeb, Estadob, CDCb, Areab, PIBb, Populaçaob, NPTb);
 
     return 0;
 }
